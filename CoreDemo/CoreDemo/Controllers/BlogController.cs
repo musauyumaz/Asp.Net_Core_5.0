9ec140f1b23a023415b5be13d942741f484ffbd1
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2022-04-23 13:09:17 +0300

    https://www.youtube.com/user/YazilimHerYerde

---
+++ CoreDemo/CoreDemo/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    
+    public class BlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogDal());
+        public IActionResult Index()
+        {
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+        public IActionResult BlogDetails(int id) 
+        {
+            var value = blogManager.GetById(id);
+            return View(value);
+        }
+    }
+}
